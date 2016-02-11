b296aec27c523464ce46e1b732f9c1169d2e4975
Eliel de Paula <elieldepaula@gmail.com>
2016-02-10 22:42:37 -0200

    Começei a preparar os comentarios para o PhpDoc

--- app/config/wpanel.php
+++ app/config/wpanel.php
@@ -1,17 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * Este arquivo contém as configurações a nível de desenvolvedor do wpanel
- * sendo que as configurações 'públicas' ficam armazenadas no arquivo config.json
- * para serem alteradas pelo administrador do site no painel de controle.
- *
- * @package Wpanel
- * @author Eliel de Paula <dev@elieldepaula.com.br>
- * @since 07/11/2014
- * Alterado em 8/02/2016
- */
-
-/**
  * Define os idiomas disponíveis. 
  */
 $config['available_languages'] = [
