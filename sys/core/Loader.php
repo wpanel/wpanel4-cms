27b0f6b8a462f8cb68ef122f5620ad1ae9711532
elieldepaula <elieldepaula@gmail.com>
2016-06-01 18:21:38 +0000

    Bug fix

--- sys/core/Loader.php
+++ sys/core/Loader.php
@@ -138,7 +138,6 @@ class CI_Loader {
 		$this->_ci_ob_level = ob_get_level();
 		$this->_ci_classes =& is_loaded();
 		log_message('info', 'Loader Class Initialized');
-		$this->wpncms();
 	}
 
 	// --------------------------------------------------------------------
@@ -1420,11 +1419,6 @@ class CI_Loader {
 
 			return $filename;
 		}
-	}
-
-	private function wpncms()
-	{
-		echo "<!-- \nDesenvolvido com Wpanel CMS - www.wpanel.com.br\n -->\n";
 	}
 
 }
