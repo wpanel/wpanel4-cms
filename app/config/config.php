c4a2b9e86f5968bc7fb263fe4a636cc274e53a0c
Eliel de Paula <elieldepaula@gmail.com>
2016-12-11 20:00:55 -0200

    Bug-fix on Main.php and changes on Config.php

--- app/config/config.php
+++ app/config/config.php
@@ -22,7 +22,10 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8000/';
+$base_url = "http://" . $_SERVER['HTTP_HOST'];
+$base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+
+$config['base_url'] = $base_url;
 
 /*
 |--------------------------------------------------------------------------
