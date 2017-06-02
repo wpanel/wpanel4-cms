9f7908683de46f98372a367e4443f8d33a773062
Eliel de Paula <elieldepaula@gmail.com>
2017-06-02 17:23:49 -0300

    - Documentation of the methods of the Users class.

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * WPanel CMS
@@ -35,7 +35,7 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
- /**
+/**
  * This class have the basic methods to manage an public user area into an website.
  * 
  * @package     WpanelCms
@@ -47,139 +47,166 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Users extends MY_Controller
 {
 
-	/**
+    /**
      * Class constructor.
      *
      * @return void
      */
-    function __construct() 
+    function __construct()
     {
-        
+
         /**
          * Here are some options provided by the MY_Controller class, you
          * can adjust as you need to your project.
          */
-        
         /**
          * Enable the CodeIgniter Profile.
          */
         // $this->wpn_profiler = TRUE;
-        
+
         /**
          * Chose the template folder.
          */
-
         // $this->wpn_template = 'default';
-        
+
         /**
          * Set the 'col' number of the mosaic views.
          */
         // $this->wpn_cols_mosaic = 3;
-        
+
         /**
          * Set the default post view: list (default) or mosaic.
          */
         // $this->wpn_posts_view = 'mosaic';
-        
+
         parent::__construct();
         $this->wpanel->check_setup();
         $this->form_validation->set_error_delimiters('<p><span class="label label-danger">', '</span></p>');
     }
 
+    /**
+     * User dashboard.
+     */
     public function index()
     {
         // Check the login.
-        if(!is_logged())
+        if (!is_logged())
             redirect('users/login');
         $this->wpanel->set_meta_title('Área de usuários');
-    	$this->render('users_index');
+        $this->render('users_index');
     }
 
+    /**
+     * Create new user account.
+     */
     public function register()
     {
         $this->form_validation->set_rules('name', 'Nome', 'required');
         $this->form_validation->set_rules('email', 'E-Mail', 'required|valid_email');
         $this->form_validation->set_rules('password', 'Senha', 'required');
         $this->form_validation->set_rules('confpass', 'Confirmação de senha', 'required|matches[password]');
-        if ($this->form_validation->run() == FALSE) {
+        if ($this->form_validation->run() == FALSE)
+        {
             $this->wpanel->set_meta_title('Cadastro de usuários');
             $this->render('users_register');
-        } else {
+        } else
+        {
             $extra_data = array(
                 'name' => $this->input->post('name'),
                 'avatar' => '',
                 'skin' => ''
             );
-            if($this->auth->create_account($this->input->post('email'), $this->input->post('password'), 'user', $extra_data))
+            if ($this->auth->create_account($this->input->post('email'), $this->input->post('password'), 'user', $extra_data))
                 redirect('users/registerok');
-            else {
-                $this->session->set_flashdata('msg_contato', 'Sua conta não pode ser criada. Verifique seus dados e tente novamente.');
+            else
+            {
+                $this->notice('Sua conta não pode ser criada. Verifique seus dados e tente novamente.', 'Erro!', 'danger');
                 redirect('users/register');
             }
         }
     }
 
+    /**
+     * Show success page to new users.
+     */
     public function registerok()
     {
         $this->wpanel->set_meta_title('Cadastro de usuários');
         $this->render('users_registerok');
     }
 
+    /**
+     * User profile.
+     */
     public function profile()
     {
         // Check the login
-        if(!is_logged())
+        if (!is_logged())
             redirect('users/login');
-        
+
         $this->form_validation->set_rules('name', 'Nome', 'required');
         $this->form_validation->set_rules('email', 'E-Mail', 'required|valid_email');
-        if ($this->form_validation->run() == FALSE) {
+        if ($this->form_validation->run() == FALSE)
+        {
             $query = $this->auth->get_account_by_id();
             $this->data_content['row'] = $query;
             $this->data_content['extra'] = (object) json_decode($query->extra_data);
             $this->wpanel->set_meta_title('Área de usuários');
             $this->render('users_profile');
-        } else {
+        } else
+        {
             $extra_data = array(
                 'name' => $this->input->post('name'),
                 'avatar' => '',
                 'skin' => ''
             );
-            if($this->auth->update_account($this->auth->get_login_data('id'), $this->input->post('email'), 'user', $extra_data))
+            if ($this->auth->update_account($this->auth->get_login_data('id'), $this->input->post('email'), 'user', $extra_data))
             {
-                $this->session->set_flashdata('msg_contato', 'Sua conta foi atualizada com sucesso!');
+                $this->notice('Sua conta foi atualizada com sucesso!', 'Sucesso!', 'success');
                 redirect('users/profile');
-            } else {
-                $this->session->set_flashdata('msg_contato', 'Sua conta não pode ser atualizada. Verifique seus dados e tente novamente.');
+            } else
+            {
+                $this->notice('Sua conta não pode ser atualizada, verifique os dados e tente novamente', 'Erro!', 'danger');
                 redirect('users/profile');
             }
         }
     }
 
+    /**
+     * Login the user.
+     */
     public function login()
     {
         $this->form_validation->set_rules('email', wpn_lang('input_email', 'Email'), 'required|valid_email');
         $this->form_validation->set_rules('password', wpn_lang('input_password', 'Password'), 'required');
-        if ($this->form_validation->run() == FALSE){
+        if ($this->form_validation->run() == FALSE)
+        {
             $this->wpanel->set_meta_title('Login de usuários');
             $this->render('users_login');
-        }
-        else {
-            if($this->auth->login($this->input->post('email'), $this->input->post('password')))
-                return redirect('users');
-            else {
-                $this->session->set_flashdata('msg_sistema', 'Seu login falhou, tente novamente.');
-                return redirect('users/login');
+        } else
+        {
+            if ($this->auth->login($this->input->post('email'), $this->input->post('password')))
+                redirect('users');
+            else
+            {
+                $this->notice('Seu login falhou, tente novamente.', 'Erro!', 'danger');
+                redirect('users/login');
             }
         }
     }
 
+    /**
+     * Logout the user.
+     */
     public function logout()
     {
-    	$this->auth->logout();
+        $this->auth->logout();
         redirect();
     }
-    
+
+    /**
+     * Change user password.
+     */
     public function change_password()
     {
         $this->form_validation->set_rules('actual_password', 'Senha atual', 'required');
@@ -187,17 +214,20 @@ class Users extends MY_Controller
         $this->form_validation->set_rules('conf_password', 'Confirmação de senha', 'required|matches[new_password]');
         if ($this->form_validation->run() == FALSE)
         {
-            redirect('users/profile');
-        } else 
+            $this->notice('Sua senha não pode ser alterada, tente novamente.', 'Erro!', 'danger');
+            return redirect('users/profile');
+        } else
         {
-            if($this->auth->change_password($this->auth->get_login_data('id'), $this->input->post('actual_password'), $this->input->post('new_password')))
+            if ($this->auth->change_password($this->auth->get_login_data('id'), $this->input->post('actual_password'), $this->input->post('new_password')))
             {
-                $this->session->set_flashdata('msg_sistema', 'Sua senha foi alterada com sucesso.');
-                return redirect('users/profile');
-            } else {
-                $this->session->set_flashdata('msg_sistema', 'Sua senha não pode ser alterada, tente novamente.');
-                return redirect('users/profile');
+                $this->notice('Sua senha foi alterada com sucesso.', 'Sucesso!', 'success');
+                redirect('users/profile');
+            } else
+            {
+                $this->notice('Sua senha não pode ser alterada, tente novamente.', 'Erro!', 'danger');
+                redirect('users/profile');
             }
         }
     }
+
 }
