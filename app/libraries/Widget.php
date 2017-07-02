82bade4e3f03607d3fa3a63e65d06b6518ef95c9
Eliel de Paula <elieldepaula@gmail.com>
2017-07-02 14:50:52 -0300

    Alteração da biblioteca Widgets.php

--- app/libraries/Widget.php
+++ app/libraries/Widget.php
@@ -3,11 +3,11 @@
 /**
  * WPanel CMS
  *
- * An open source Content Manager System for blogs and websites using CodeIgniter and PHP.
+ * An open source Content Manager System for websites and systems using CodeIgniter.
  *
  * This content is released under the MIT License (MIT)
  *
- * Copyright (c) 2014 - 2016, British Columbia Institute of Technology
+ * Copyright (c) 2008 - 2017, Eliel de Paula.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -29,47 +29,156 @@
  *
  * @package     WpanelCms
  * @author      Eliel de Paula <dev@elieldepaula.com.br>
- * @copyright   Copyright (c) 2008 - 2016, Eliel de Paula. (https://elieldepaula.com.br/)
+ * @copyright   Copyright (c) 2008 - 2017, Eliel de Paula. (https://elieldepaula.com.br/)
  * @license     http://opensource.org/licenses/MIT  MIT License
- * @link        https://wpanelcms.com.br
+ * @link        https://wpanel.org
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
+define('EXT', '.php');
 
 /**
- * Widget Plugin Class
+ * Esta classe provê os métodos para o funcionamento do mecanismo de Widget usado
+ * no WpanelCMS.
  * 
- * Provides a way to use widgets into WpanelCms
- * 
- * @package     WpanelCms
- * @subpackage  Libraries
- * @category    Libraries
- * @version:     0.0.1
+ * @author Eliel de Paula <dev@elieldepaula.com.br>
+ * @since v.1.0.0
  */
 class Widget
 {
-	
-    public $module_path;
 
-    function __get($var) {
+    function __get($var)
+    {
         global $CI;
         return $CI->$var;
     }
 
-    function runit($file ,$param = null) 
-    {        
-        $args = func_get_args();
+    /**
+     * Initialize the params to the widget.
+     * 
+     * @param array $params
+     * @return $this
+     */
+    public function initialize($params = array())
+    {
+        foreach ($params as $key => $val)
+        {
+            if (isset($this->$key))
+            {
+                $method = 'set_' . $key;
+                if (method_exists($this, $method))
+                    $this->$method($val);
+                else
+                    $this->$key = $val;
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Load an widget file.
+     * 
+     * @param string $file
+     * @param array $param
+     * @return mixed
+     */
+    public function load($file, $param = null)
+    {
+        //print_r($file);
         $module = '';
-        /* is module in filename? */
-        if (($pos = strrpos($file, '/')) !== FALSE) {
+        if (($pos = strrpos($file, '/')) !== FALSE)
+        {
             $module = substr($file, 0, $pos);
             $file = substr($file, $pos + 1);
         }
-        list($path, $file) = Modules::find($file, $module, 'widgets/');
+
+        list($path, $file) = $this->_find($file, $module, 'widgets/');
+
         if ($path === FALSE)
-            $path = APPPATH.'widgets/';
-        Modules::load_file($file, $path);
+            $path = APPPATH . 'widgets/';
+
         $file = ucfirst($file);
+
+        $this->_load_file($file, $path);
+
         $widget = new $file($param);
-        return $widget->run();
+        return $widget->main();
     }
-} 
+
+    /**
+     * Find some file into the folders.
+     * 
+     * @param String $file
+     * @param String $module
+     * @param String $base
+     * @return Mixed
+     */
+    private function _find($file, $module, $base)
+    {
+        $segments = explode('/', $file);
+
+        $file = array_pop($segments);
+        $file_ext = (pathinfo($file, PATHINFO_EXTENSION)) ? $file : $file . EXT;
+
+        $path = ltrim(implode('/', $segments) . '/', '/');
+        $module ? $modules[$module] = $path : $modules = array();
+
+        if (!empty($segments))
+        {
+            $modules[array_shift($segments)] = ltrim(implode('/', $segments) . '/', '/');
+        }
+
+        foreach (array(APPPATH . 'modules/' => '../modules/') as $location => $offset)
+        {
+            foreach ($modules as $module => $subpath)
+            {
+                $fullpath = $location . $module . '/' . $base . $subpath;
+                if ($base == 'libraries/' OR $base == 'models/')
+                {
+                    if (is_file($fullpath . ucfirst($file_ext)))
+                        return array($fullpath, ucfirst($file));
+                }
+                else
+                /* load non-class files */
+                if (is_file($fullpath . $file_ext))
+                    return array($fullpath, $file);
+            }
+        }
+        return array(FALSE, $file);
+    }
+
+    /**
+     * Load some file.
+     * 
+     * @param string $file
+     * @param string $path
+     * @param string $type
+     * @param bool $result
+     * @return mixed
+     */
+    private function _load_file($file, $path, $type = 'other', $result = TRUE)
+    {
+        $file = str_replace(EXT, '', $file);
+        $location = $path . $file . EXT;
+        if ($type === 'other')
+        {
+            if (class_exists($file, FALSE))
+            {
+                log_message('debug', "File already loaded: {$location}");
+                return $result;
+            }
+            include_once $location;
+        } else
+        {
+            /* load config or language array */
+            include $location;
+
+            if (!isset($$type) OR ! is_array($$type))
+                show_error("{$location} does not contain a valid {$type} array");
+
+            $result = $$type;
+        }
+        log_message('debug', "File loaded: {$location}");
+        return $result;
+    }
+
+}
