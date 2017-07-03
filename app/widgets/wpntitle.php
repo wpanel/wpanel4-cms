2f752ba628ba321df64b2574fbeaf8d08e1c9d7c
Eliel de Paula <elieldepaula@gmail.com>
2017-07-03 13:12:12 -0300

    Alteração de licença em alguns arquivos
    - config/auth.php
    - config/wpanel.php
    - widgets/wpntitle.php

--- app/widgets/wpntitle.php
+++ app/widgets/wpntitle.php
@@ -1,57 +1,28 @@
 <?php
 
 /**
- * WPanel CMS
- *
- * An open source Content Manager System for websites and systems using CodeIgniter.
- *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2008 - 2017, Eliel de Paula.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package     WpanelCms
- * @author      Eliel de Paula <dev@elieldepaula.com.br>
- * @copyright   Copyright (c) 2008 - 2017, Eliel de Paula. (https://elieldepaula.com.br/)
- * @license     http://opensource.org/licenses/MIT  MIT License
- * @link        https://wpanel.org
+ * @copyright Eliel de Paula <dev@elieldepaula.com.br>
+ * @license http://wpanel.org/license
  */
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * Title class.
+ * Classe Wpntitle.
  * 
  * @author Eliel de Paula <dev@elieldepaula.com.br>
- * @since v1.0.0
  */
 class Wpntitle extends Widget
 {
 
     /**
-     * Main method of the widget.
+     * Método principal do widget.
      * 
-     * @return mixed
+     * @return string
      */
     public function main()
     {
-        return "<title>" . wpn_config('site_titulo') . "</title>\n";
+        return "<title>" . $this->wpanel->get_meta_title() . "</title>\n";
     }
 
 }
