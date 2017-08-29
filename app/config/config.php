43b7c8adb221bf831b40148c08820973f9da1ccc
Eliel de Paula <elieldepaula@gmail.com>
2017-08-29 12:46:26 -0300

    Alteração da configuração de BASE_URL

--- app/config/config.php
+++ app/config/config.php
@@ -22,10 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$base_url = "http://" . $_SERVER['HTTP_HOST'];
-$base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
-
-$config['base_url'] = $base_url;
+$config['base_url'] = BASE_URL;
 
 /*
 |--------------------------------------------------------------------------
