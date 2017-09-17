8260ab6455ab422d180159c325d76357123367bc
Eliel de Paula <elieldepaula@gmail.com>
2017-09-17 15:57:32 -0300

    Arquivos de tradução para o módulo admin Developers

---
+++ app/modules/admin/language/portuguese/wpn_developers_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @copyright Eliel de Paula <dev@elieldepaula.com.br>
+ * @license http://wpanel.org/license
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Arquivo de idioma do módulo admin Developers.
+ *
+ * @language Portuguese
+ * @author Eliel de Paula <dev@elieldepaula.com.br>
+ */
+
+$lang['module_title'] = "Desenvolvedor";
+$lang['module_description'] = "Opções específicas para desenvolvedores.";
+$lang['module_log_list'] = "Lista de logs";
+$lang['module_log_reading'] = "Visualização de logs";
+$lang['log_filename'] = "Arquivo";
