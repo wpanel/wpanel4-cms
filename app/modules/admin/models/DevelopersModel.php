c413258e949dd11c223a8cef31cfae0b92119630
Eliel de Paula <elieldepaula@gmail.com>
2017-10-02 23:29:27 -0300

    Modulo Developers
    - Adicionado index.html na pasta admin/models;
    - Documentação do model DevelopersModel.php

--- app/modules/admin/models/DevelopersModel.php
+++ app/modules/admin/models/DevelopersModel.php
@@ -1,12 +1,24 @@
 <?php
 
 /**
- * Description of Developers
- *
- * @author elieldepaula
+ * @copyright Eliel de Paula <dev@elieldepaula.com.br>
+ * @license http://wpanel.org/license
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Classe model DevelopersModel.
+ * 
+ * @author Eliel de Paula <dev@elieldepaula.com.br>
  */
 class DevelopersModel extends CI_Model {
     
+    /**
+     * Retorna o número da versão.
+     * 
+     * @return bigint
+     */
     public function get_migration_version()
     {
         $this->db->select('version');
@@ -14,6 +26,11 @@ class DevelopersModel extends CI_Model {
         return $query->row()->version;
     }
     
+    /**
+     * Retorna a lista de arquivos de migração.
+     * 
+     * @return array
+     */
     public function get_migration_list()
     {
         $migration_files = $this->migration->find_migrations();
