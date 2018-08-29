5e78da3a19af2a309d325c345a25a508fd4d5f4c
Eliel de Paula <elieldepaula@gmail.com>
2018-08-29 16:09:46 -0300

    Revisão do Setup inicial

--- app/modules/setup/controllers/Main.php
+++ app/modules/setup/controllers/Main.php
@@ -4,20 +4,17 @@
  * @copyright Eliel de Paula <dev@elieldepaula.com.br>
  * @license http://wpanel.org/license
  */
-
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * This class creates a new admin user if the account table accounts was empty. Usually this occurs in the
- * first run of Wpanel CMS.
+ * This class creates a initial database and a new admin user if the account 
+ * table accounts was empty. Usually this occurs in the first run of Wpanel CMS
+ * using the url [base-url]/index.php/setup
  *
  * @author Eliel de Paula <dev@elieldepaula.com.br>
  */
-class Main extends CI_Controller
+class Main extends MY_Controller
 {
-
-    var $layout_vars = array();
-    var $content_vars = array();
 
     /**
      * Class constructor.
@@ -32,10 +29,10 @@ class Main extends CI_Controller
      */
     public function index()
     {
-        // Gera o banco de dados inicial.
+        // Generate the initial database.
         $this->load->library('migration');
         $this->migration->latest();
-        
+
         // Check if accounts is empty.
         if ($this->auth->accounts_empty() == FALSE)
             redirect('admin/login');
@@ -45,10 +42,10 @@ class Main extends CI_Controller
         $this->form_validation->set_rules('email', 'Valid email', 'required|valid_email');
         $this->form_validation->set_rules('agree', 'Terms of use', 'required');
 
-        if ($this->form_validation->run() == FALSE)
-            $this->load->view('setup/index', $this->layout_vars);
-        else
-        {
+        if ($this->form_validation->run() == FALSE) {
+            $this->set_var('title', 'Setup');
+            $this->render();
+        } else {
             // Set App token.
             $this->load->model('configuracao');
             $this->load->library('wpanel');
@@ -59,22 +56,18 @@ class Main extends CI_Controller
             // Register the root user.
             $result = $this->auth->register(
                     $this->input->post('email', TRUE), $this->input->post('password', TRUE), 'ROOT', array(
-                    'name' => $this->input->post('name'),
-                    'skin' => 'blue',
-                    'avatar' => ''
-                )
-            );
-            if ($result > 0)
-            {
-
+                        'name' => $this->input->post('name'),
+                        'skin' => 'blue',
+                        'avatar' => ''
+                    )
+                );
+            if ($result > 0) {
                 // Activate the first user account.
                 $this->auth->activate($result);
-
-                $this->session->set_flashdata('msg_sistema', 'Account succefull created');
+                $this->session->set_flashdata('msg_sistema', 'Account succefull created.');
                 redirect('admin/login');
-            } else
-            {
-                $this->session->set_flashdata('msg_sistema', 'Can´t create this account');
+            } else {
+                $this->session->set_flashdata('msg_sistema', 'Can´t create this account.');
                 redirect('setup');
             }
         }
--- app/modules/setup/views/theme/index.php
+++ app/modules/setup/views/theme/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Admin WPanel | <?= $title; ?></title>
+        <title>WPanel | <?= $title; ?></title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <link rel="shortcut icon" type="image/x-icon" href="<?= base_url('/lib/img/favicon.ico'); ?>">
         <!-- bootstrap 3.3.4 -->
