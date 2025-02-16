d51280d6deea35fa66ea5b25c130a54bf88b6b80
Eliel de Paula <elieldepaula@gmail.com>
2025-02-16 23:23:51 +0100

    chore: Add new translations

---
+++ app/language/english/theme_default_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @copyright Eliel de Paula <dev@elieldepaula.com.br>
+ * @license http://wpanel.org/license
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['bot_toggle_navigation'] = 'Toggle navigation';
+$lang['header_search'] = 'Search';
+$lang['header_category'] = 'Category';
+$lang['header_about'] = 'About';
+$lang['header_newsletter'] = 'Newsletter';
+$lang['header_social'] = 'Social media';
+$lang['header_comments'] = 'Comments';
+$lang['header_search_results'] = 'Search results for: ';
+$lang['header_description'] = 'Description';
+$lang['keep_reading'] = 'Keep reading...';
+$lang['placeholder_search'] = 'Search...';
---
+++ app/language/portuguese/theme_default_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @copyright Eliel de Paula <dev@elieldepaula.com.br>
+ * @license http://wpanel.org/license
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['bot_toggle_navigation'] = 'Alternar navegação';
+$lang['header_search'] = 'Buscar';
+$lang['header_category'] = 'Categoria';
+$lang['header_about'] = 'Sobre';
+$lang['header_newsletter'] = 'Newsletter';
+$lang['header_social'] = 'Social';
+$lang['header_comments'] = 'Comentários';
+$lang['header_search_results'] = 'Resultados para: ';
+$lang['header_description'] = 'Descrição';
+$lang['keep_reading'] = 'Continuar lendo...';
+$lang['placeholder_search'] = 'Buscar...';
